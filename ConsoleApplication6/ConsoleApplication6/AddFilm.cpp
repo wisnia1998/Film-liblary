a447f3f6edbae129ada48a494900e196db32af4a
wisnia1998 <48693427+wisnia1998@users.noreply.github.com>
2019-05-13 19:45:54 +0200

    edycja
    Edytowanie danych w bazie

--- ConsoleApplication6/ConsoleApplication6/AddFilm.cpp
+++ ConsoleApplication6/ConsoleApplication6/AddFilm.cpp
@@ -32,7 +32,7 @@ void AddFilm::addFilm()
 	query = "INSERT INTO filmy VALUES (NULL, '" + title + "'," + "'" + autor + "'," + "'" + data + "'," + "'" + length + "'," + "'" +  mark +"')";
 	mysql_init(&mysql);
 	std::cout << query;
-	mysql_real_connect(&mysql, "127.0.0.1", "root", "", "filmoteka", 0, NULL, 0);
+	mysql_real_connect(&mysql, "127.0.0.1", "root", "", "filmoteka", 0, NULL, 0);//³¹czenie z baz¹
 	mysql_select_db(&mysql, "filmoteka");
 	mysql_query(&mysql, query.c_str());
 	
